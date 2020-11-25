5b55b34b1246fa4947268315997afd1bd948af1f
nicolapv <nicola.p.v.a@gmail.com>
2020-11-26 00:16:28 +0100

    last changes Nikola

---
+++ Assets/Scripts/FireStick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireStick : MonoBehaviour
+{
+
+    Collider stickCollider;
+    //fireprefab = Resources.Load<GameObject>($"Prefabs/{prefabName}");
+
+
+    // Start is called before the first frame update
+    Renderer rend;
+    void Start() 
+    {
+        stickCollider = GetComponent<Collider>();
+        stickCollider.isTrigger = true;
+    
+    }
+
+
+    // Update is called once per frame
+    void Update() { }
+   
+ //   void OnTriggerStay(Collider other)
+  //  {
+  //      if (other.attachedRigidbody)
+   //     {
+            // destroy stick
+    //        Destroy(gameObject);
+
+   //     }
+  //  }
+
+}
---
+++ Assets/Scripts/InstructionScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int lifeTime = 10;
+    public  void Start()
+    {
+        StartCoroutine(WaitThenDie());
+    }
+    IEnumerator WaitThenDie()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+}
